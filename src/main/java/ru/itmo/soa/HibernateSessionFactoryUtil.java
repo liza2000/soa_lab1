eddca14ddffd76e6885b70ff226d6fddafcb1e75
Georgii Savin <gsavin@devexperts.com>
2021-09-15 12:40:43 +0300

    migration to maven

--- src/main/java/ru/itmo/soa/HibernateSessionFactoryUtil.java
+++ src/main/java/ru/itmo/soa/HibernateSessionFactoryUtil.java
@@ -1,16 +1,17 @@
 package ru.itmo.soa;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
 
 import java.io.File;
 
 class HibernateSessionFactoryUtil {
     private static SessionFactory sessionFactory;
 
-    private HibernateSessionFactoryUtil() {}
+    private HibernateSessionFactoryUtil() {
+    }
 
-     static SessionFactory getSessionFactory(String deploymentDirectoryPath) {
+    static SessionFactory getSessionFactory(String deploymentDirectoryPath) {
         if (sessionFactory == null) {
             try {
                 File file = new File("F:\\4course\\soa\\lab1\\web\\WEB-INF\\hybernate.cfg.xml");
